8e27bc5925819f4b921d3e8cfe6d204d0e87c9e9
Michael Pell <MichaelCPell@gmail.com>
2012-06-13 13:17:25 -0400

    with JS refactoring

--- app/assets/javascripts/decks.js
+++ app/assets/javascripts/decks.js
@@ -1,19 +1,22 @@
 
 //This function adds fields in _form
-$(document).ready(function(){
-  $('form').on('click', '.add_fields', function(event) {
-    var regexp, time;
-    time = new Date().getTime();
-    regexp = new RegExp($(this).data('id'), 'g');
-    $(this).before($(this).data('fields').replace(regexp, time));
-    return event.preventDefault();
+function augmentFormToAddFields(){
+  $(document).ready(function(){
+    $('form').on('click', '.add_fields', function(event) {
+      var regexp, time;
+      time = new Date().getTime();
+      regexp = new RegExp($(this).data('id'), 'g');
+      $(this).before($(this).data('fields').replace(regexp, time));
+      return event.preventDefault();
+    });
   });
-});
+}
 
 
 
 //This function allows the users to control with the keyboard arrows
-$(document).keydown(function(e){
+function mapKeys(){
+  $(document).keydown(function(e){
 
       if (e.keyCode == 37) {
          $("#previous-button").click();
@@ -30,7 +33,49 @@ $(document).keydown(function(e){
          return false;
       }
 
-});
+  });
+}
 
+
+//The Function Used for flipping a card
+function flipCard(){
+    clickCounter = 0;
+      $("#card-partial").click(function(){
+
+        if (clickCounter == 1){
+          clickCounter = -1;
+          $("a#next-button").click();
+        }
+
+        backOfCard = $("#card-partial-content h1").data('back');
+        backOfCard == "" ? $("#card-partial-content h1").html("Back is Blank") : $("#card-partial-content h1").html(backOfCard);
+
+        clickCounter += 1;
+    })
+}
+
+
+//The function responsible for changing the instruction header
+function changeInstruction(){
+    currentCard = $("#current-card").html();
+    deckTotal = $("#deck-total").html();
+
+    $("#card-partial").click(function(){
+      if( currentCard === deckTotal ){
+          $("#instruction-bar h4").text("You've Finished the Deck!");
+      }else{
+          $("#instruction-bar h4").text("Click Again to Continue");
+      }
+    });
+}
+
+
+//call functions
+$(document).ready( function(){
+  changeInstruction();
+  flipCard();
+  mapKeys();
+  augmentFormToAddFields();
+});
 
 
